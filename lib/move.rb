2ab00045782e9bd6e9d121f3ae669e7e00c4ceb7
Mike Pokalsky <mpoksky1989@gmail.com>
2021-03-02 01:13:40 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,7 +11,8 @@ def input_to_index(input)
   input.to_i - 1
 end
 
-board = [" ", " ", " "]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 def move(board, index, marker = "X")
   board[index.to_i] = marker
 end
